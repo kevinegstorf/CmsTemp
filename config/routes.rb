9ed2768ae4776a0cee2165626fae2c175a9ef91c
kevinegstorf <kevin.egstorf@gmail.com>
2015-08-31 20:47:49 +0200

    added CMS

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
+
+  # Make sure this routeset is defined last
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +58,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+    comfy_route :cms_admin, :path => '/cms'
+    comfy_route :cms_admin, :path => '/admin'
+    comfy_route :cms, :path => '/', :sitemap => false
 end
