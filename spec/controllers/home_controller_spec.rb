a9f7129c29c6439059084b8f1d0c680f52909843
kevinegstorf <kevin.egstorf@gmail.com>
2015-09-01 21:49:34 +0200

    updated spec file :koala:

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  specify { expect(response).to render_template "home/index" }
+  it "directs to homepage index" do
+    get :index
+    expect(response).to render_template("index")
+  end
 
+ describe 'Get #index' do
+   specify{ expect(response).to render_template :index }
+ end
 end
